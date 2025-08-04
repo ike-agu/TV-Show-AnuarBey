22f30a8205bd91bc290696b3f984dd508706ab62
Anuar Duisenbek <coolboy949494@gmail.com>
2025-08-04 16:47:35 +0100

    Add episode-list container and render EC with template

--- script.js
+++ script.js
@@ -12,6 +12,24 @@ function makePageForEpisodes(episodeList) {
   header.id = "episode-count";
   header.textContent = `Got ${episodeList.length} episode(s)`;
   rootElem.appendChild(header);
+
+  const episodeListElem = document.createElement("div");
+  episodeListElem.id = "episode-list";
+  rootElem.appendChild(episodeListElem);
+
+  const template = document.getElementById("episode-template");
+
+  episodeList.forEach((episode) => {
+    const episodeCard = template.content.cloneNode(true);
+    episodeCard.querySelector(".episode-img").src = episode.image.medium;
+    episodeCard.querySelector(".episode-title").textContent = episode.name;
+    episodeCard.querySelector(".episode-code").textContent = `S${String(
+      episode.season
+    ).padStart(2, "0")}E${String(episode.number).padStart(2, "0")}`;
+    episodeCard.querySelector(".episode-summary").innerHTML = episode.summary;
+
+    episodeListElem.appendChild(episodeCard);
+  });
 }
 
 window.onload = setup;
