f09a9cea4220a8d325fffca2d632976b5b709654
ike-agu <ikennaww@yahoo.fr>
2025-08-08 19:41:47 +0100

    Add explanatory comments to code

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //You can edit ALL of the code here
-let allEpisodes = []
+let allEpisodes = []; // store all episodes globally so that other functions can access it.
 function setup() {
   allEpisodes = getAllEpisodes();
   makePageForEpisodes(allEpisodes);
@@ -14,35 +14,36 @@ function makePageForEpisodes(episodeList) {
   header.textContent = `Got ${episodeList.length} episode(s)`;
   rootElem.appendChild(header);
 
+//create div to hold all episode cards
   const episodeListElem = document.createElement("div");
   episodeListElem.id = "episode-list";
   rootElem.appendChild(episodeListElem);
 
+  //clone and fill episode details from template
   const template = document.getElementById("episode-template");
 
   episodeList.forEach((episode) => {
     const episodeCard = template.content.cloneNode(true);
 
     episodeCard.querySelector(".episode-title").textContent = episode.name;
-    episodeCard.querySelector(".episode-code").textContent = `S${String(
-      episode.season
-    ).padStart(2, "0")}E${String(episode.number).padStart(2, "0")}`;
+    episodeCard.querySelector(".episode-code").textContent = `S${String(episode.season).padStart(2, "0")}E${String(episode.number).padStart(2, "0")}`;
     episodeCard.querySelector(".episode-summary").innerHTML = episode.summary;
     episodeCard.querySelector(".episode-img").src = episode.image.medium;
     episodeListElem.appendChild(episodeCard);
   });
 }
-
-function render(){
-  const filteredEpisodes = allEpisodes.filter(function (episode){
+//filters episodes based on user search and re-render the page.
+function render() {
+  const filteredEpisodes = allEpisodes.filter(function (episode) {
     return episode.name.toLowerCase().includes(input.value.toLowerCase());
-  })
+  });
   makePageForEpisodes(filteredEpisodes);
 }
 
-const input = document.querySelector("input")
-   input.addEventListener("keyup", function(){
-    render()
-  })
+//Get input field and listen to user type
+const input = document.querySelector("input");
+input.addEventListener("keyup", function () {
+  render();
+});
 
 window.onload = setup;
